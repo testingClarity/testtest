6a435e2cbe17d965e66e5f48beec6868c57c22e4
Fernando Doglio <deleteman@gmail.com>
2016-03-01 13:45:45 +0000

    File atributo-para-iniciativa-externa-1.R created

---
+++ atributo-para-iniciativa-externa-1.R
@@ -0,0 +1,11 @@
+#!/usr/bin/Rscript
+
+library(claritySDK)
+
+
+build_table <- function()
+{
+    # data <- data.frame(num = 1:3, name = 'Hello world!')
+    # tabn <- clarity_outputname()
+    # write_hive(data, tabn)
+}
